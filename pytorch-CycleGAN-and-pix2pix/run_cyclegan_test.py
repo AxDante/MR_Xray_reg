db7e8223f54c36560d1b4575aa0cd04dac695e3e
AxDante <knight16729438@gmail.com>
2021-03-23 15:05:49 -0400

    testing script update

--- pytorch-CycleGAN-and-pix2pix/run_cyclegan_test.py
+++ pytorch-CycleGAN-and-pix2pix/run_cyclegan_test.py
@@ -38,6 +38,19 @@ from util import html
 if __name__ == '__main__':
     opt = TestOptions().parse()  # get test options
     # hard-code some parameters for test
+
+    target_dir = os.path.abspath(__file__ + "/../../../")
+    print('parent dir: ', target_dir)
+
+    opt.dataroot = os.path.join(target_dir, 'result/AVN_NMDID_thickness_3_format_jpg_date0315')
+    #opt.dataroot = os.path.join(target_dir, 'result/')
+    print('dataroot dir: ', opt.dataroot)
+    opt.name = 'AVN_NMDID_thickness_3_format_jpg_date0321_onlyIdentity'
+    opt.model = 'cycle_gan'
+    opt.checkpoints_dir = os.path.join(target_dir, 'checkpoints')
+    opt.results_dir = os.path.join(target_dir, 'tv_test_results/' + opt.name)
+    print('results_dir dir: ', opt.results_dir)
+
     opt.num_threads = 0   # test code only supports num_threads = 0
     opt.batch_size = 1    # test code only supports batch_size = 1
     opt.serial_batches = True  # disable data shuffling; comment this line if results on randomly chosen images are needed.
@@ -58,13 +71,16 @@ if __name__ == '__main__':
     if opt.eval:
         model.eval()
     for i, data in enumerate(dataset):
-        print(data.shape)
+        print('i ',i)
+        print('data', data)
         if i >= opt.num_test:  # only apply our model to opt.num_test images.
             break
         model.set_input(data)  # unpack data from data loader
         model.test()           # run inference
         visuals = model.get_current_visuals()  # get image results
+        print('visuals ', visuals)
         img_path = model.get_image_paths()     # get image paths
+
         if i % 5 == 0:  # save images to an HTML file
             print('processing (%04d)-th image... %s' % (i, img_path))
         save_images(webpage, visuals, img_path, aspect_ratio=opt.aspect_ratio, width=opt.display_winsize)
